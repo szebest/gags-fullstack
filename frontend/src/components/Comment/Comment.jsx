7198fc4c5f561e1442b09022e69ac3818d0b89cc
szebestik@gmail.com <szebestik@gmail.com>
2022-01-04 22:22:09 +0100

    Created an option to edit an comment, Comment displays now information about the time it has been last changed and if it was edited #17

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,13 +1,19 @@
 import classes from './styles/Comment.module.scss'
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import NewComment from '../NewComment/NewComment'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-export default function Comment({postID, comment, sendComment, updateThisComment, index}) {
+export default function Comment({ postID, comment, sendComment, updateThisComment, index }) {
     const [openReply, setOpenReply] = useState(false)
+    const [openOptions, setOpenOptions] = useState(false)
+    const [editing, setEditing] = useState(false)
+    const [lastEdited, setLastEdited] = useState("")
+    const optionsRef = useRef()
+    const modalRef = useRef()
+    const [textEntered, setTextEntered] = useState("")
     const [action, setAction] = useState({
         like: comment.actionType === 'like' ? 1 : 0,
         dislike: comment.actionType === 'dislike' ? 1 : 0
@@ -24,12 +30,12 @@ export default function Comment({postID, comment, sendComment, updateThisComment
             method: "GET",
             url: `http://localhost:3001/user/avatar/${comment.author}`,
         })
-        .then(res => {
-            setAvatar(res.data.imgSrc)
-        })
-        .catch(err => {
-            console.error(err)
-        })
+            .then(res => {
+                setAvatar(res.data.imgSrc)
+            })
+            .catch(err => {
+                console.error(err)
+            })
     }, [comment])
 
     useEffect(() => {
@@ -38,7 +44,7 @@ export default function Comment({postID, comment, sendComment, updateThisComment
         const like = action.like - tmpAction.like
         const dislike = action.dislike - tmpAction.dislike
 
-        tmpSetAction({...action})
+        tmpSetAction({ ...action })
 
         if (like === 0 && dislike === 0) return
 
@@ -49,13 +55,34 @@ export default function Comment({postID, comment, sendComment, updateThisComment
                 "Authorization": `Bearer ${Cookies.get("accessToken")}`
             }
         })
-        .then(res => {
-            updateThisComment(res.data.updatedComment, index)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then(res => {
+                updateThisComment(res.data.updatedComment, index)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [action])
+
+    const handleClick = (e) => {
+        if (!openOptions && optionsRef.current && modalRef.current && !optionsRef.current.contains(e.target) && !modalRef.current.contains(e.target)) {
+            setOpenOptions(false)
+        }
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('click', handleClick)
+
+        return () => {
+            document.body.removeEventListener('click', handleClick)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (editing === true) {
+            setTextEntered(comment.comment)
+        }
+        setOpenOptions(false)
+    }, [editing])
 
     function sendCommentForward(comment, parentComment) {
         setOpenReply(false)
@@ -69,17 +96,93 @@ export default function Comment({postID, comment, sendComment, updateThisComment
         tmpState.dislike = 0
         tmpState.like = tmpState.like === 0 ? 1 : 0
 
-        setAction({...tmpState})
+        setAction({ ...tmpState })
     }
 
     function dislike() {
         if (!hasAccess) return
-        
+
         const tmpState = action
         tmpState.like = 0
         tmpState.dislike = tmpState.dislike === 0 ? 1 : 0
 
-        setAction({...tmpState})
+        setAction({ ...tmpState })
+    }
+
+    function sendUpdateRequest() {
+        axios.patch(`http://localhost:3001/posts/${postID}/comment/${comment._id}`, {
+            comment: textEntered
+        }, {
+            headers: {
+                "Authorization": `Bearer ${Cookies.get("accessToken")}`
+            }
+        })
+            .then(res => {
+                updateThisComment(res.data.updatedComment, index)
+                setOpenReply(false)
+                setEditing(false)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+
+    const treatAsUTC = (date) => {
+        const result = new Date(date)
+        result.setMinutes(result.getMinutes() - result.getTimezoneOffset())
+
+        return result
+    }
+
+    useEffect(() => {
+        const dateNow = new Date()
+        const commentTimestamp = comment.timestamp
+
+        const millisecondsPerMinute = 60 * 1000
+        const differenceInMinutes = Math.ceil((treatAsUTC(dateNow) - treatAsUTC(commentTimestamp)) / millisecondsPerMinute)
+
+        if (differenceInMinutes < 60) {
+            setLastEdited(differenceInMinutes + (differenceInMinutes === 1 ? " minute ago" : " minutes ago"))
+            return
+        }
+
+        const millisecondsPerHour = 60 * 60 * 1000
+        const differenceInHours = Math.ceil((treatAsUTC(dateNow) - treatAsUTC(commentTimestamp)) / millisecondsPerHour)
+
+        if (differenceInHours < 24) {
+            setLastEdited(differenceInHours + (differenceInHours === 1 ? " hour ago" : " hours ago"))
+            return
+        }
+
+        const millisecondsPerDay = 24 * 60 * 60 * 1000
+        const differenceInDays = Math.ceil((treatAsUTC(dateNow) - treatAsUTC(commentTimestamp)) / millisecondsPerDay)
+
+        setLastEdited(differenceInDays + (differenceInDays === 1 ? " day ago" : " days ago"))
+    }, [comment])
+
+    if (editing) {
+        return (
+            <div className={classes.commentSplit}>
+                <div key={comment._id} className={classes.commentContainer} >
+                    <div className={classes.profilePictureHolder}>
+                        <img alt="avatar" width="28" height="28" src={avatar} />
+                    </div>
+                    <div className={classes.contentHolder}>
+                        <div className={classes.author}>
+                            <a target="_blank" href={`/profile/${comment.author}`}>{comment.author}</a>
+                        </div>
+                    </div>
+                </div>
+                <textarea value={textEntered} onChange={(e) => setTextEntered(e.target.value)} />
+                <div className={classes.actionButtons}>
+                    <button onClick={() => {
+                        setEditing(false)
+                        sendUpdateRequest()
+                    }}>Edit</button>
+                    <button onClick={() => setEditing(false)}>Abort</button>
+                </div>
+            </div>
+        )
     }
 
     return (
@@ -100,6 +203,10 @@ export default function Comment({postID, comment, sendComment, updateThisComment
                     <div className={classes.contentHolder}>
                         <div className={classes.author}>
                             <a target="_blank" href={`/profile/${comment.author}`}>{comment.author}</a>
+                            <p>{lastEdited}</p>
+                            {comment.edited && 
+                                <p><i>Edited</i></p>
+                            }
                         </div>
                         <div className={classes.comment}>{comment.comment}</div>
                     </div>
@@ -116,10 +223,25 @@ export default function Comment({postID, comment, sendComment, updateThisComment
                     <div onClick={() => setOpenReply(prev => !prev)} className={`${classes.controlContainer} ${classes.hoverBackground}`}>
                         <p>Reply</p>
                     </div>
+                    <div className={classes.relativeWrapper}>
+                        {openOptions &&
+                            <div ref={modalRef} className={classes.optionsContainerContent}>
+                                <p onClick={() => setEditing(true)}>Edit Comment</p>
+                                <p>Delete Comment</p>
+                            </div>
+                        }
+                        {comment.isAuthor === true &&
+                            <div ref={optionsRef} onClick={() => setOpenOptions(prev => !prev)} className={`${classes.optionsContainer} ${classes.controlContainer} ${classes.hoverBackground}`}>
+                                <div className={classes.optionsContainerStyle}></div>
+                                <div className={classes.optionsContainerStyle}></div>
+                                <div className={classes.optionsContainerStyle}></div>
+                            </div>
+                        }
+                    </div>
                 </div>
                 <div>
-                    {openReply && 
-                    <NewComment sendComment={sendCommentForward} parentComment={comment._id} />
+                    {openReply &&
+                        <NewComment sendComment={sendCommentForward} parentComment={comment._id} />
                     }
                 </div>
             </div>
